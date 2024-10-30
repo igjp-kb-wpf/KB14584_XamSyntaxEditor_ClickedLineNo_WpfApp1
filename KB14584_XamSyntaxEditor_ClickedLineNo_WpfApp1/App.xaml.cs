2a81f0ffd6783f1517e6b6acbbd0b4119349c211
norikois <33947244+norikois@users.noreply.github.com>
2024-10-30 11:36:21 +0900

    Initial Commit
    Initial Commit

---
+++ KB14584_XamSyntaxEditor_ClickedLineNo_WpfApp1/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KB14584_XamSyntaxEditor_ClickedLineNo_WpfApp1;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
